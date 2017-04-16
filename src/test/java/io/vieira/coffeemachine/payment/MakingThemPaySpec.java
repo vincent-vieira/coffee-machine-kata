db7429e0391c856bfc2eb507f18dc36d4f1056fd
Vincent Vieira <vincent.vieira@carbon-it.com>
2017-04-16 19:38:03 +0200

    feat(payment): first draft of test structure

---
+++ src/test/java/io/vieira/coffeemachine/payment/MakingThemPaySpec.java
@@ -0,0 +1,16 @@
+package io.vieira.coffeemachine.payment;
+
+import org.junit.Test;
+
+public class MakingThemPaySpec {
+
+    @Test
+    public void brewingDrinksWithEnoughMoneyShouldReturnThem() {
+
+    }
+
+    @Test
+    public void brewingDrinksWithoutEnoughMoneyShouldShowAMessage() {
+
+    }
+}
