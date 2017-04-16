a356f7f7924fc47fa48efde68b6942d32a30e645
Vincent Vieira <vincent.vieira@carbon-it.com>
2017-04-16 18:52:51 +0200

    feat(maker): first draft of test structure

---
+++ src/test/java/io/vieira/coffeemachine/MakingDrinksSpec.java
@@ -0,0 +1,36 @@
+package io.vieira.coffeemachine;
+
+import org.junit.Test;
+
+public class MakingDrinksSpec {
+
+    @Test
+    public void drinkMakerShouldMakeAllKindOfDrinksWithAStick() {
+
+    }
+
+    @Test
+    public void drinkMakerShouldMakeAllKindOfDrinksWithoutAStick() {
+
+    }
+
+    @Test
+    public void drinkMakerShouldMakeAllKindOfDrinksWithSugar() {
+
+    }
+
+    @Test
+    public void drinkMakerShouldMakeAllKindOfDrinksWithoutSugar() {
+
+    }
+
+    @Test
+    public void drinkMakerShouldMakeAllKindOfDrinksWithSugarAndAStick() {
+
+    }
+
+    @Test
+    public void drinkMakerShouldForwardMessages() {
+
+    }
+}
