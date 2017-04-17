1e907320b13ea56018e5b0a91ae8fdb99233eca7
Vincent Vieira <vincent.vieira@carbon-it.com>
2017-04-17 15:43:06 +0200

    feat(reporting): prettyprint for default handling of reporting

--- src/main/java/io/vieira/coffeemachine/reporting/DrinksSalesReporter.java
+++ src/main/java/io/vieira/coffeemachine/reporting/DrinksSalesReporter.java
@@ -3,14 +3,19 @@ package io.vieira.coffeemachine.reporting;
 import io.vieira.coffeemachine.instruction.Drink;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @FunctionalInterface
 public interface DrinksSalesReporter {
 
-    DrinksSalesReporter NOOP = (brewedDrinksSummary, totalEarned) -> {};
+    DrinksSalesReporter DEFAULT = (brewedDrinksSummary, totalEarned) -> System.out.printf(
+            "%f earned brewing %s",
+            totalEarned,
+            brewedDrinksSummary.entrySet().stream().map(entry -> String.format("%d %ss", entry.getValue(), entry.getKey().name().toLowerCase())).collect(Collectors.joining(","))
+    );
 
     /**
-     * Allow to handle a coffee machine report.
+     * Allows to handle a coffee machine report.
      *
      * @param brewedDrinksSummary a map containing for each {@link Drink} type, the number of drinks made yet so far.
      * @param totalEarned the amount of money earned.
